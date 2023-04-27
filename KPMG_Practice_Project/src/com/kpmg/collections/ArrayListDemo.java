6c1a01a3b54c8dbf0dc630557503517c93cd5cb5
Aditi Roy <aditiroy492@gmail.com>
2023-04-27 10:42:00 +0530

    Adding New Project

--- KPMG_Practice_Project/src/com/kpmg/collections/ArrayListDemo.java
+++ KPMG_Practice_Project/src/com/kpmg/collections/ArrayListDemo.java
@@ -5,6 +5,7 @@ package com.kpmg.collections;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 /**
  * @author Administrator
  *
@@ -17,18 +18,57 @@ public class ArrayListDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		List list = new ArrayList();
+		// define the List with Array List Impl 
 		
-		System.out.println("Complete Size of the application --> " + list.size());
-		
-		list.add("Kolkata");
-		list.add("Mumbai");
-		list.add("BAnGalOre");
-		list.add("Nagpur");
-		list.add(101);
-		
-		System.out.println("Complete Size of the application --> " + list.size());
-
+				List<String> list =new ArrayList<String>();
+				
+				// size of collection ! 
+				
+				System.out.println("complete Size of Collection-->" +list.size());
+				
+				
+				// add the element in colection 
+				
+				// Add 
+				list.add("mumbai");
+				list.add("MALE");
+				list.add("NEw YoRK");
+				list.add("mumbai");
+				list.add("channai");
+				list.add("Blore");
+				
+				
+				System.out.println("complete Size of  final Collection-->" +list.size());
+				
+				
+				// Remove  by index & by name 
+				
+				list.remove("MALE");
+				
+				list.remove(3);
+				
+				System.out.println("complete Size of  Collection after removal-->" +list.size());
+				
+				
+				// List of collection printing :--> 
+				
+				// there are two ways we can use it
+				
+				//old way
+				Iterator<String> i=list.iterator();
+				while(i.hasNext()) {
+					
+					  String test=i.next();
+					  System.out.println("list of collection-->" +test);
+					
+				}
+				
+				// morden way to put the details 
+				
+				for(String value: list) {
+					
+					System.out.println("Element:--" +value);
+				}
 	}
 
 }
